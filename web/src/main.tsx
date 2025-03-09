e7510f6dd12639294507d17e2f3ee8edd22cb330
bideiro <qd-deocampo@tip.edu.ph>
2025-03-09 14:00:39 +0800

    cleaned one file in web

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from "react";
+// import React, { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+// import App from "./app.tsx";
+
+// createRoot(document.getElementById("root")!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+
+import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import React from "react";
+import LiveFeed from "./components/LiveFeed";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <LiveFeed />
+    </div>
   </StrictMode>
 );
